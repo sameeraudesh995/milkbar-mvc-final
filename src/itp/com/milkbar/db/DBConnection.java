cb5c055bd7a3827f9013dd6acbdf6b1b564eddb3
sameeraudesh995 <sameeraudesh.2225@gmail.com>
2025-01-18 23:51:57 +0530

    DBConnection created

---
+++ src/itp/com/milkbar/db/DBConnection.java
@@ -0,0 +1,26 @@
+package itp.com.milkbar.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private static DBConnection dbConnection;
+    private Connection conn;
+    private DBConnection() throws ClassNotFoundException,
+            SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        conn= DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/pos_v1",
+                "root",
+                "1234"
+        );
+    }
+    public static DBConnection getInstance() throws SQLException, ClassNotFoundException {
+        return dbConnection==null?
+                dbConnection=new DBConnection():dbConnection;
+    }
+    public Connection getConnection() {
+        return conn;
+    }
+}
